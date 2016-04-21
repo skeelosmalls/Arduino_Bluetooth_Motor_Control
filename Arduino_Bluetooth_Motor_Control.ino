31889e1534bc54aaf68535b5a851fc0d5af8fcff
SkeeloSmalls <skeelo.smalls@gmail.com>
2016-04-21 10:35:49 -0500

    first working script.

---
+++ Arduino_Bluetooth_Motor_Control.ino
@@ -0,0 +1,73 @@
+#include <SoftwareSerial.h>
+//#include <Servo.h>
+#include <AFMotor.h>
+
+int bluetoothTx = 1;
+int bluetoothRx = 0;
+AF_DCMotor motor(1);
+
+SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
+//
+//int Motor1A = 8;
+//int Motor1B = 9;
+//int Motor2A = 10;
+//int Motor2B = 11;
+
+void setup()
+{
+  bluetooth.begin(115200);
+  bluetooth.print("$$$");
+  delay(100);
+  bluetooth.println("U,9600,N");
+  bluetooth.begin(9600);
+  // turn on motor
+  motor.setSpeed(25);
+  motor.run(RELEASE);
+}
+int flag1 = -1;
+int flag2 = -1;
+
+void loop()
+{
+  if (bluetooth.available())
+  {
+    char toSend = (char)bluetooth.read();
+
+    //    Serial.print(toSend);
+    if (toSend == 'S') //Stops Motors
+    {
+      flag1 = 0;
+      flag2 = 0;
+      motor.run(RELEASE);
+    }
+    if (toSend == 'F' || toSend == 'G' || toSend == 'I') // Moves Motor Forward
+    {
+      if (flag1 != 1)
+      {
+        flag1 = 1;
+        //          digitalWrite( Motor1A, HIGH );
+        //          analogWrite( Motor1B, 50 );
+        motor.run(FORWARD);
+      }
+    }
+    if (toSend == 'B' || toSend == 'H' || toSend == 'J') //Moves Motor Backwards
+    {
+      if (flag1 != 2)
+      {
+        flag1 = 2;
+        motor.run(BACKWARD);
+      }
+    }
+    if (toSend == 'C') // Close Hand Test
+    {
+      if (flag1 != 1)
+      {
+        flag2 = 1;
+        motor.run(FORWARD);
+        delay(500);
+        motor.run(RELEASE);
+      }
+    }
+
+  }
+}
